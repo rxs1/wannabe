64bbec2584ffb24b52ce61c7b5b29387acc935d1
rxs1 <sr.saufi@gmail.com>
2016-12-26 16:00:30 +0700

    fix header

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<?php 
+	$title = "WannaBe Profesional Course";
+	include 'head.php';
+	?>
+</head>
+<body>
+	
+
+	<?php 
+	include 'nav.php';
+	include 'content-home.php';
+	?>
+
+	<?php 
+	include 'footer.php';
+	?>
+	
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a id="logo" class="navbar-brand" href="#"><span><img id="logo-img" height="45" width="45" src="assets/img/logo.png"></span>WannaBe</a>
+    </div>
+    <ul id="menu" class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+  
+    </ul>
+   
+      <a id="login" class="btn btn-primary pull-right" href="#" data-toggle="modal" data-target="#myModal"><span><i class="glyphicon glyphicon-user"></i></span>  Login Now</a>
+   
+  </div>
+</nav>
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+    
+      
+      <div class="modal-body">
+        <a class="btn btn-danger pull-right" data-dismiss="modal">      
+        <button type="button" class=" close" >&times;</button>
+        </a>
+        <div class="container">
+          <div class="row">
+
+              <div class="col-md-10 col-md-offset-1">
+                <h3>Choose your desired account:</h3>
+                <a href="#"><img src="assets/img/login-with.png"></a>  
+              </div>
+          </div>
+        </div>
+
+      </div>
+    
+    </div>
+
+  </div>
+</div>
